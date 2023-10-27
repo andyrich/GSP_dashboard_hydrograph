2faa7456328dd95b1dc765e880d1e22027e37dcf
Andrew Rich <>
2023-10-27 11:27:29 -0700

    querying hydrograph now works with: http://127.0.0.1:8053/?station_name=Son0001

--- app_v6.py
+++ app_v6.py
@@ -2,7 +2,7 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 import urllib.parse
-
+import wiski_data
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
@@ -19,7 +19,7 @@ def update_input_from_url(search):
     if search:
         # Parse the URL to extract the 'input_string' parameter
         params = urllib.parse.parse_qs(search[1:])
-        input_string = params.get('input_string', ['DefaultString'])[0]
+        input_string = params.get('station_name', ['DefaultString'])[0]
         return input_string
     else:
         return 'DefaultString'
@@ -30,8 +30,12 @@ def update_input_from_url(search):
 )
 def update_plot(input_string):
     # Create an example Plotly figure (customize as needed)
-    import plotly.express as px
-    fig = px.scatter(x=[1, 2, 3, 4], y=[1, 2, 3, 4], title=f'Plot for {input_string}')
+    # import plotly.express as px
+    # fig = px.scatter(x=[1, 2, 3, 4], y=[1, 2, 3, 4], title=f'Plot for {input_string}')
+    x =wiski_data.wiski_plot(input_string)
+    x.get_station_pars(remove_pt=True)
+    fig = x.plot_gw()
+
     return fig
 
 if __name__ == '__main__':
