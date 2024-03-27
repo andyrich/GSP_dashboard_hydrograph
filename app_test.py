6285c8f0e504235575e3e0a3a37891bfec53e2fa
andyrich <arich@scwa.ca.gov>
2024-03-27 10:48:37 -0700

    works with only pressure transducer data

--- app_test.py
+++ app_test.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import dash
 from dash import dcc, html, Input, Output, State
 import dash_table
+import wiski_data
 import pressure_transducers
 
 '''
@@ -10,13 +11,15 @@ shows a map, hydrograph of pressure transducers
 '''
 
 # Sample DataFrame
-df = pressure_transducers.layout()
+df = pressure_transducers.get_recent_measurements()
 df = df.astype({"station_longitude": float,
                 "station_latitude": float})
 
+df = df.drop(columns = ['ts_name',"parametertype_id",'parametertype_name','from','to','station_id'])
+
 # Rename latitude and longitude columns
 df.rename(columns={'station_latitude': 'Station_Latitude', 'station_longitude': 'Station_Longitude',
-                   'station_name': "Station_Name"}, inplace=True)
+                   'station_name': "Station_Name", "Elapsed Time":"Elapsed_Time"}, inplace=True)
 
 # Initialize the Dash app
 app = dash.Dash(__name__)
@@ -30,9 +33,13 @@ app.layout = html.Div([
         id='datatable',
         columns=[{'name': col, 'id': col} for col in df.columns],
         data=df.to_dict('records'),
-        row_selectable='multi'
+        row_selectable='multi',
+        filter_action='native',
+        sort_action='native',
+        style_table={'overflowX': 'auto'}
     )
 ])
+
 
 
 # Define callback to update map based on table selection
@@ -44,11 +51,25 @@ def update_map(selected_rows):
     if selected_rows:
         selected_cities = df.iloc[selected_rows]['Station_Name']
         selected_rows = df[df['Station_Name'].isin(selected_cities)]
-        fig = px.scatter_geo(selected_rows, lat='Station_Latitude', lon='Station_Longitude', hover_name='Station_Name')
-    else:
-        fig = px.scatter_geo(df, lat='Station_Latitude', lon='Station_Longitude', hover_name='Station_Name')
+        fig = px.scatter_mapbox(  selected_rows, lon='Station_Longitude', lat='Station_Latitude',
+                             color="Number of Months Since Last Measurement",
+                             hover_name="Station_Name",
+                             hover_data=["Station_Name", "station_no", "Number of Months Since Last Measurement"],
+                             size="Number of Months Since Last Measurement",
 
-    fig.update_geos(projection_type="natural earth")
+                             )
+
+    else:
+
+        fig = px.scatter_mapbox(  df, lon='Station_Longitude', lat='Station_Latitude',
+                             color="Number of Months Since Last Measurement",
+                             hover_name="Station_Name",
+                             hover_data=["Station_Name", "station_no", "Number of Months Since Last Measurement"],
+                             size="Number of Months Since Last Measurement",
+                             )
+
+    fig.update_layout(mapbox_style="open-street-map")
+
     return fig
 
 
@@ -64,13 +85,17 @@ def update_scatter_plot(clickData, n_clicks, selected_rows):
     triggered_id = ctx.triggered[0]['prop_id'] if ctx.triggered else None
 
     if triggered_id == 'map-graph.clickData' and clickData:
-        selected_point = clickData['points'][0]
-        lat = selected_point['lat']
-        lon = selected_point['lon']
-        selected_row = df[(df['Station_Latitude'] == lat) & (df['Station_Longitude'] == lon)]
-        fig = px.scatter(selected_row, x='Station_Latitude', y='Station_Longitude', text='Station_Name')
-        fig.update_traces(marker=dict(size=12, color='red'), selector=dict(mode='markers'))
-        fig.update_layout(showlegend=False)
+        selected_point = clickData['points'][0]['hovertext']
+        print(selected_point)
+        x = wiski_data.wiski_plot(selected_point)
+        x.get_station_pars(remove_pt=False)
+        fig = x.plot_gw(plot_wet = False, seasonal=False, RMP=False, isw = False)
+        # lat = selected_point['lat']
+        # lon = selected_point['lon']
+        # selected_row = df[(df['Station_Latitude'] == lat) & (df['Station_Longitude'] == lon)]
+        # fig = px.scatter(selected_row, x='Station_Latitude', y='Station_Longitude', text='Station_Name')
+        # fig.update_traces(marker=dict(size=12, color='red'), selector=dict(mode='markers'))
+        # fig.update_layout(showlegend=False)
         return fig
     elif triggered_id == 'scatter-button.n_clicks' and n_clicks > 0 and selected_rows:
         selected_cities = df.iloc[selected_rows]['Station_Name']
